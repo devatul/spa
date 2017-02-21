8487dd3656f74c3ec0d81c4f0f4095b9c180e692
arun <arun.etech2011@gmail.com>
2017-02-21 17:27:11 +0530

    rsync configuration

--- web/Shipitfile.js
+++ web/Shipitfile.js
@@ -1,26 +1,23 @@
 var path = require('path');
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
-/*****************
+/**********************
 this is the dommy configration data need to change with actual when implented
 **********************/
   shipit.initConfig({
     default: {
-      workspace: path.resolve('./tmp/nubity-spa-deploy'),
-      repositoryUrl: 'https://github.com/nubity/spa.git ',
-      ignore: ['.git', 'node_modules'],
-      keepReleases: 5,
-      deleteOnRollback: false,
-      shallowClone: true,
+      rsync: [
+        '--include',' "js/bundle.js"',
+        '--exclude',' "js/*"',
+        '--exclude', '"node_modules"',
+     ],
     },
     production: {
       servers: 'etech@144.76.34.244:4444',
-      deployTo: 'public_html/spa',
       key: path.resolve('./server-key.ppk'),
     },
     develop: {
       servers: 'etech00@192.168.1.124',
-      deployTo: path.resolve('./tmp/dev.deploy')
     },
   });
 
@@ -29,8 +26,8 @@ this is the dommy configration data need to change with actual when implented
   });
 
   shipit.on('updated', function () {
-    var buildDirectory = path.resolve('./js/bundle.js');
-    var serverDirectory = 'public_html/spa/web/js';
+    var buildDirectory = path.resolve('../web/');
+    var serverDirectory = 'public_html/spa/web/test/';
     shipit.remoteCopy(buildDirectory, serverDirectory);
   });
 
