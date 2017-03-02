5a520ffdf520ec3d121921954034aab45ce7bc6b
arun <arun.etech2011@gmail.com>
2017-03-02 13:38:39 +0530

    shipit improved

--- web/Shipitfile.js
+++ web/Shipitfile.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var ssh = require('./js/config/config');
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
 
@@ -12,7 +13,7 @@ module.exports = function (shipit) {
     },
     production: {
       servers: 'etech@144.76.34.244:4444',
-      key: path.resolve('./server-key.ppk'),
+      key: path.resolve(ssh.SSH_key_path),
     },
     develop: {
       servers: 'etech00@192.168.1.124',
@@ -24,7 +25,7 @@ module.exports = function (shipit) {
   });
 
   shipit.on('updated', function () {
-    var buildDirectory = path.resolve('../web/');
+    var buildDirectory = path.resolve('../web/*');
     var serverDirectory = 'public_html/spa/web/test/';
     shipit.remoteCopy(buildDirectory, serverDirectory);
   });
--- web/js/config/prod.js
+++ web/js/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
   baseURL: 'https://api.pricing.nubity.com',
+  SSH_key_path: './server-key.ppk',
 };
