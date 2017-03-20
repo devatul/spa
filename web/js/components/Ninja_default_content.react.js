31fa2d80587ad2267da6ca383c280ceac9e96925
arun <arun.etech2011@gmail.com>
2017-03-20 18:16:14 +0530

    [SPA-24 older] remaining colors added to ticket icon

--- web/js/components/Ninja_default_content.react.js
+++ web/js/components/Ninja_default_content.react.js
@@ -128,11 +128,15 @@ module.exports = React.createClass({
         tooltip = <Tooltip id="tooltip" style={{textTransform: 'capitalize'}}>{ticket[key].status}</Tooltip>;
         status = 'icon nb-ticket icon-state ';
         if ('open' == ticket[key].status) {
-          status = status + 'blue-text';
+          status = status + 'black-text';
         } else if ('closed' == ticket[key].status) {
-          status = status + 'green-text';
-        } else if ('in-progress' == ticket[key].status) {
           status = status + 'grey-text';
+        } else if ('in-progress' == ticket[key].status) {
+          status = status + 'blue-text';
+        } else if ('resolved' == ticket[key].status) {
+          status = status + 'green-text';
+        } else if ('on-hold' == ticket[key].status) {
+          status = status + 'yellow-text';
         }
 
         if ('billing' == ticket[key].department) {
