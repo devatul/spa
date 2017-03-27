9e0ded46983f1aeb664f92dcd28687ef30b918f4
Delfina <delfinapolito@gmail.com>
2017-03-27 14:38:00 -0300

    when clicking other templates sometimes doesnt work

--- web/js/components/Plugins_carousel.react.js
+++ web/js/components/Plugins_carousel.react.js
@@ -42,26 +42,26 @@ module.exports = React.createClass({
           <CarouselItem>
           <div className="col-sm-8 col-sm-offset-2">
             <div className={templates[key] !== undefined ? 'col-sm-3' : 'hidden'}>
-              <a className={templates[key] !== undefined && this.state.active == templates[key].template ? 'carousel-active thumbnail' : 'thumbnail'} >
-                <img src={templates[key] !== undefined ? templates[key].logo : ''} alt="Image" className="img-responsive" onClick={this.clickTemplate.bind(this, templates[key])}/>
+              <a className={templates[key] !== undefined && this.state.active == templates[key].template ? 'carousel-active thumbnail' : 'thumbnail'} onClick={this.clickTemplate.bind(this, templates[key])}>
+                <img src={templates[key] !== undefined ? templates[key].logo : ''} alt="Image" className="img-responsive"/>
                 <div className="is-installed-container"><span className={templates[key] !== undefined && true == templates[key].is_installed ? 'is-installed' : 'hidden'}></span></div>
               </a>
             </div>
             <div className={templates[key+1] !== undefined ? 'col-sm-3' : 'hidden'}>
-              <a className={templates[key+1] !== undefined && this.state.active == templates[key+1].template ? 'carousel-active thumbnail' : 'thumbnail'} >
-                <img src={templates[key+1] !== undefined ? templates[key+1].logo : ''} className="img-responsive" onClick={this.clickTemplate.bind(this, templates[key+1])}/>
+              <a className={templates[key+1] !== undefined && this.state.active == templates[key+1].template ? 'carousel-active thumbnail' : 'thumbnail'} onClick={this.clickTemplate.bind(this, templates[key+1])}>
+                <img src={templates[key+1] !== undefined ? templates[key+1].logo : ''} className="img-responsive"/>
                 <div className="is-installed-container"><span className={templates[key+1] !== undefined && true == templates[key+1].is_installed ? 'is-installed' : 'hidden'}></span></div>
               </a>
             </div>
             <div className={templates[key+2] !== undefined ? 'col-sm-3' : 'hidden'}>
-              <a className={templates[key+2] !== undefined && this.state.active == templates[key+2].template ? 'carousel-active thumbnail' : 'thumbnail'} >
-                <img src={templates[key+2] !== undefined ? templates[key+2].logo : ''} alt="Image" className="img-responsive" onClick={this.clickTemplate.bind(this, templates[key+2])}/>
+              <a className={templates[key+2] !== undefined && this.state.active == templates[key+2].template ? 'carousel-active thumbnail' : 'thumbnail'} onClick={this.clickTemplate.bind(this, templates[key+2])}>
+                <img src={templates[key+2] !== undefined ? templates[key+2].logo : ''} alt="Image" className="img-responsive"/>
                 <div className="is-installed-container"><span className={templates[key+2] !== undefined && true == templates[key+2].is_installed ? 'is-installed' : 'hidden'}></span></div>
               </a>
             </div>
             <div className={templates[key+3] !== undefined ? 'col-sm-3' : 'hidden'}>
-              <a className={templates[key+3] !== undefined && this.state.active == templates[key+3].template ? 'carousel-active thumbnail' : 'thumbnail'} >
-                <img src={templates[key+3] !== undefined ? templates[key+3].logo : ''} alt="Image" className="img-responsive" onClick={this.clickTemplate.bind(this, templates[key+3])}/>
+              <a className={templates[key+3] !== undefined && this.state.active == templates[key+3].template ? 'carousel-active thumbnail' : 'thumbnail'} onClick={this.clickTemplate.bind(this, templates[key+3])}>
+                <img src={templates[key+3] !== undefined ? templates[key+3].logo : ''} alt="Image" className="img-responsive"/>
                 <div className="is-installed-container"><span className={templates[key+3] !== undefined && true == templates[key+3].is_installed ? 'is-installed' : 'hidden'}></span></div>
               </a>
             </div>
