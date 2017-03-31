3fc8d73f62d61159bd392525b3530171cf310db7
Delfina <delfinapolito@gmail.com>
2017-03-31 14:58:06 -0300

    Integrating Intercom

--- web/js/components/Ninja_support_section.react.js
+++ web/js/components/Ninja_support_section.react.js
@@ -3,8 +3,40 @@ var Router                     = require('../router');
 var redirect                   = require('../actions/RouteActions').redirect;
 var SessionStore               = require('../stores/SessionStore');
 var NinjaDefaultContent        = require('./Ninja_default_content.react');
+var getCompanyInfo             = require('../actions/RequestActions').getCompanyInfo;
 
 module.exports = React.createClass({
+  getInitialState: function () {
+    var companyInfo = SessionStore.getCompanyInfo();
+    return {
+      companyName: companyInfo.name,
+    };
+  },
+
+  componentWillMount: function () {
+    if (!SessionStore.isLoggedIn()) {
+      redirect('login');
+    }
+  },
+  componentDidMount: function () {
+    getCompanyInfo();
+    SessionStore.addChangeListener(this._onChange);
+    $('#intercom-container').removeClass('hidden');
+  },
+
+  componentWillUnmount: function () {
+    SessionStore.removeChangeListener(this._onChange);
+    $('#intercom-container').addClass('hidden');
+  },
+
+  _onChange: function () {
+    if (this.isMounted()) {
+      var companyInfo = SessionStore.getCompanyInfo();
+      this.setState({
+        companyName: companyInfo.name,
+      });
+    }
+  },
 
   _createTicket: function () {
     redirect('create_ticket');
@@ -14,16 +46,21 @@ module.exports = React.createClass({
     redirect('live_chat');
   },
 
-  componentWillMount: function () {
-    if (!SessionStore.isLoggedIn()) {
-      redirect('login');
-    }
-  },
-
   render: function () {
+    var name = localStorage.getItem('nubity-firstname') + ' ' + localStorage.getItem('nubity-lastname');
+    var email = localStorage.getItem('nubity-user-email');
+    
     if (!SessionStore.isLoggedIn()) {
       return (<div></div>);
     }
+
+    window.Intercom('boot', {
+      app_id: 'xs6j43ab',
+      name: name, 
+      email: email, 
+      created_at: Math.ceil(Date.now()/1000),
+      'company_name': this.state.companyName,
+    });
 
     return (
       <div className="principal-section">
