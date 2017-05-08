e6bb9be649f3b492683a240bde72c85dc6a2df84
Delfina <delfinapolito@gmail.com>
2017-05-08 17:13:10 -0300

    cursor pointer in graph close

--- web/js/components/Forgot_password.react.js
+++ web/js/components/Forgot_password.react.js
@@ -3,6 +3,7 @@ var Router                     = require('../router');
 var redirect                   = require('../actions/RouteActions').redirect;
 var SessionStore               = require('../stores/SessionStore');
 var ForgotPasswordAction       = require('../actions/RequestActions').forgotPassword;
+var Preloader                  = require('./Preloader.react');
 
 class ForgotPassword extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class ForgotPassword extends React.Component {
     this.state = {
       message:      '',
       messageClass: 'hidden',
+      submit:       (<button className="action-button nubity-blue" type="submit">Reset password</button>),
     };
     this._onSubmit = this._onSubmit.bind(this);
     this.closeAlert = this.closeAlert.bind(this);
@@ -19,21 +21,27 @@ class ForgotPassword extends React.Component {
     e.preventDefault();
     var email = this.refs.email.value;
     if ('' !== email) {
+      this.setState({
+        submit: (<div className="login-load"><Preloader size="medium" /></div>),
+      });
       ForgotPasswordAction(email).then(function (res) {
         this.setState({
           message:      res,
           messageClass: 'alert alert-success alert-margin',
+          submit:       (<button className="action-button nubity-blue" type="submit">Reset password</button>),
         });
       }.bind(this)).catch(function (res) {
         this.setState({
           message:      res.message,
           messageClass: 'alert alert-danger alert-margin',
+          submit:       (<button className="action-button nubity-blue" type="submit">Reset password</button>),
         });
       }.bind(this));
     } else {
       this.setState({
         message:      'Please enter email',
         messageClass: 'alert alert-danger alert-margin',
+        submit:       (<button className="action-button nubity-blue" type="submit">Reset password</button>),
       });
     }
   }
@@ -49,23 +57,33 @@ class ForgotPassword extends React.Component {
     redirect('login');
   }
 
+  _redirectTerms() {
+    redirect('terms-and-conditions');
+  }
+
+  _redirectPolicy() {
+    redirect('privacy-policies');
+  }
+
   render() {
     return (
       <section className="login-div">
-        <div className="col-lg-4 col-lg-offset-4 login-box">
+        <div className="login-box">
           <div className="top-div">
-            <button className="go-to-signup" onClick={this._redirectLogin}>Go to login</button>
+            <a className="action-button nubity-blue" onClick={this._redirectLogin}>Go to login</a>
           </div>
-          <div className="login-logo"></div>
-          <p className="login-title">Your cloud, managed</p>
-          <p className="login-subtitle">Sign in to check all your Clouds, Servers, Devices and Apps.</p>
-          <form className="login-form" onSubmit={this._onSubmit}>
-            <div className="row">
-              <div className={this.state.messageClass} role="alert">
-                <button type="button" className="close" onClick={this.closeAlert} aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                {this.state.message}
-              </div>
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1">
+              <div className="login-logo"></div>
             </div>
+          </div>
+          <div className="row">
+            <div className={this.state.messageClass} role="alert">
+              <button type="button" className="no-margin close" onClick={this.closeAlert} aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <p>{this.state.message}</p>
+            </div>
+          </div>
+          <form className="login-form" onSubmit={this._onSubmit}>
             <div className="form-group row">
               <div className="input-group col-xs-10 col-xs-offset-1">
                 <div className="input-group-addon">
@@ -74,8 +92,16 @@ class ForgotPassword extends React.Component {
                 <input type="email" className="form-control no-shadow" id="email" placeholder="Email" ref="email" name="email" required />
               </div>
             </div>
-            <button className="col-xs-12 login-button" type="submit" onClick={this._onSubmit}>Reset Password</button>
+            <div onChange={this._onChange}>{this.state.submit}</div>
           </form>
+          <div className="login-footer">
+            <div className="col-xs-6">
+              <a className="link-login" onClick={this._redirectTerms}>Terms and conditions</a>
+            </div>
+            <div className="col-xs-6">
+              <a className="link-login" onClick={this._redirectPolicy}>Privacy policies</a>
+            </div>
+          </div>
         </div>
       </section>
     );
